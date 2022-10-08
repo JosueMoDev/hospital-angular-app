4e22764931419d7d5770bb5cc0657e29cc07a786
Jonas Morales <jonasjosuemo2021@outlook.com>
2022-10-08 17:01:25 -0600

    prod env added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@
 //TODO: change to url prod
 export const environment = {
   production: true,
-  baseUrl: 'https://hospital-manage-application.herokuapp.com/api'
+  baseUrl: 'https://hospital-manage-backend.herokuapp.com/api'
 };
